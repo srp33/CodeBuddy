5c0ff8ad6887cd5b16df139249334ee6d76b7eb5
srp33 <stephen_piccolo@byu.edu>
2020-05-12 10:50:45 -0600

    Removed unnecessary downloads

--- front_end/download_aux_files.sh
+++ front_end/download_aux_files.sh
@@ -1,13 +1,7 @@
 #! /bin/bash
 
-##wget -O stylesheet.css https://cdnjs.cloudflare.com/ajax/libs/bulma/0.7.5/css/bulma.min.css
 #wget -O fontawesome.js https://use.fontawesome.com/releases/v5.3.1/js/all.js
-#wget -O jquery.min.js https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js
-##wget -O bulma-dashboard.min.css https://github.com/lucperkins/bulma-dashboard/blob/master/dist/bulma-dashboard.min.css
-#wget -O bulmaswatch.min.css https://jenil.github.io/bulmaswatch/flatly/bulmaswatch.min.css
-
-#wget -O stylesheet.css https://unpkg.com/buefy/dist/buefy.min.css
-##wget https://vuejs.org/js/vue.min.js
+wget -O jquery.min.js https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js
 
 # From https://jenil.github.io/bulmaswatch/flatly/#blog/
 wget https://jenil.github.io/bulmaswatch/flatly/_overrides.scss
@@ -15,3 +9,5 @@ wget https://jenil.github.io/bulmaswatch/flatly/_variables.scss
 wget https://jenil.github.io/bulmaswatch/flatly/bulmaswatch.min.css
 wget https://jenil.github.io/bulmaswatch/flatly/bulmaswatch.min.css.map
 wget https://jenil.github.io/bulmaswatch/flatly/bulmaswatch.scss
+
+
