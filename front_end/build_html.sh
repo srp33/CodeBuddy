47fd93af427178fc59ee898ed5e8b22d0d00aeba
e171505 <e171505@lscodebuddy.byu.edu>
2020-07-08 09:55:29 -0600

    Summarize logs page

--- front_end/build_html.sh
+++ front_end/build_html.sh
@@ -18,6 +18,8 @@ cat header.html navbar_top.html navbar_course.html navbar_assignment.html navbar
 cat header.html navbar_top.html navbar_course.html navbar_assignment.html navbar_problem.html navbar_bottom.html container_top.html delete_problem.html container_bottom_other.html container_bottom_all.html footer.html > /app/delete_problem.html
 cat header.html navbar_top.html navbar_course.html navbar_assignment.html navbar_problem.html navbar_bottom.html container_top.html view_answer.html container_bottom_other.html container_bottom_all.html footer.html > /app/view_answer.html
 cat header.html navbar_top.html navbar_course.html navbar_bottom.html container_top.html permissions.html container_bottom_other.html container_bottom_all.html footer.html > /app/permissions.html
+cat header.html navbar_top.html navbar_course.html navbar_bottom.html container_top.html summarize_logs.html container_bottom_other.html container_bottom_all.html footer.html > /app/summarize_logs.html
+
 
 cat header.html container_top.html error.html container_bottom_other.html container_bottom_all.html footer.html > /app/error.html
 
