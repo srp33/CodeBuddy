1750771804a7d57668d0cf6dbf56be5e040684b0
srp33 <srp33@codebuddy.byu.edu>
2020-07-24 11:54:26 -0600

    Middle layer removes any orphan temp directories. Also uses gunicorn.

--- middle_layer/main.py
+++ middle_layer/main.py
@@ -1,7 +1,9 @@
+import arrow
 import base64
 from fastapi import FastAPI, HTTPException
 import getpass
 import os
+from pathlib import Path
 from pydantic import BaseModel
 import shutil
 import subprocess
@@ -24,17 +26,24 @@ def hello():
 @app.post("/exec/")
 def exec(info: ExecInfo):
     base_tmp_dir_path = f"/tmp/codebuddy_backend_{getpass.getuser()}"
-    os.makedirs(base_tmp_dir_path, exist_ok=True)
-    tmp_dir_path = tempfile.mkdtemp(dir=base_tmp_dir_path)
     cpus = 1
+    tmp_dir_path = None
 
     try:
+        # This is meant to identify any old temp directories that inadvertently were not deleted.
+        # This is not the best design, but we do it this way for simplicity.
+        # It means we don't need to configure a separate process to run in the background.
+        # And it should be fast enough.
+        remove_old_temp_dirs(base_tmp_dir_path)
+
+        os.makedirs(base_tmp_dir_path, exist_ok=True)
+        tmp_dir_path = tempfile.mkdtemp(dir=base_tmp_dir_path)
+
         # Save the user's code to a file that will be accessible inside the container.
         with open(f"{tmp_dir_path}/code", "w") as the_file:
             the_file.write(info.code)
 
         # About --cap-drop: https://www.redhat.com/en/blog/secure-your-containers-one-weird-trick
-        #docker_command = f"timeout -s 9 {info.timeout_seconds}s docker run --rm --user $(id -u):$(id -g) --cpus {cpus} --memory={info.memory_allowed_mb}m --cap-drop=ALL --read-only=true --log-driver=none --workdir /sandbox -v {tmp_dir_path}/:/sandbox/ {info.image_name} /sandbox/code {info.output_type}"
         docker_command = f"timeout -s 9 {info.timeout_seconds}s docker run --rm --user $(id -u):$(id -g) --cpus {cpus} --memory={info.memory_allowed_mb}m --cap-drop=ALL --log-driver=none --workdir /sandbox -v {tmp_dir_path}/:/sandbox/ {info.image_name} /sandbox/code {info.output_type}"
 
         result = subprocess.run(docker_command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
@@ -72,9 +81,19 @@ def exec(info: ExecInfo):
         error_occurred = True
         output = traceback.format_exc()
     finally:
-        shutil.rmtree(tmp_dir_path, ignore_errors=True)
+        if tmp_dir_path:
+            shutil.rmtree(tmp_dir_path, ignore_errors=True)
 
     return {"output": output, "error_occurred": error_occurred}
 
 def encode_image_bytes(b):
     return str(base64.b64encode(b), "utf-8")
+
+# From https://stackoverflow.com/questions/12485666/python-deleting-all-files-in-a-folder-older-than-x-days
+def remove_old_temp_dirs(dir_path):
+    criticalTime = arrow.now().shift(minutes=-5)
+
+    for item in Path(dir_path).glob("*"):
+        itemTime = arrow.get(item.stat().st_mtime)
+        if itemTime < criticalTime:
+            shutil.rmtree(item, ignore_errors=True)
