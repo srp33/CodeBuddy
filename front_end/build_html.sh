7e040fed6d18173e4e004dd5c52e3e2d3ba4851c
e171505 <e171505@lscodebuddy.byu.edu>
2020-08-25 10:17:41 -0600

    Removed navbar_assignment.html from course pages

--- front_end/build_html.sh
+++ front_end/build_html.sh
@@ -6,10 +6,11 @@ cd /tmp
 
 cat header.html navbar_top.html navbar_course.html navbar_bottom.html container_top.html home.html container_bottom.html footer.html > /app/home.html
 cat header.html navbar_top.html navbar_bottom.html container_top.html login.html container_bottom.html footer.html > /app/login.html
-cat header.html navbar_top.html navbar_course.html navbar_assignment.html navbar_bottom.html container_top.html course.html container_bottom.html footer.html > /app/course.html
-cat header.html navbar_top.html navbar_course.html navbar_assignment.html navbar_bottom.html container_top.html edit_course.html container_bottom.html footer.html > /app/edit_course.html
-cat header.html navbar_top.html navbar_course.html navbar_assignment.html navbar_bottom.html container_top.html delete_course.html container_bottom.html footer.html > /app/delete_course.html
-cat header.html navbar_top.html navbar_course.html navbar_assignment.html navbar_bottom.html container_top.html delete_course_submissions.html container_bottom.html footer.html > /app/delete_course_submissions.html
+cat header.html navbar_top.html navbar_course.html navbar_bottom.html container_top.html course.html container_bottom.html footer.html > /app/course.html
+cat header.html navbar_top.html navbar_course.html navbar_bottom.html container_top.html course_admin.html container_bottom.html footer.html > /app/course_admin.html
+cat header.html navbar_top.html navbar_course.html navbar_bottom.html container_top.html edit_course.html container_bottom.html footer.html > /app/edit_course.html
+cat header.html navbar_top.html navbar_course.html navbar_bottom.html container_top.html delete_course.html container_bottom.html footer.html > /app/delete_course.html
+cat header.html navbar_top.html navbar_course.html navbar_bottom.html container_top.html delete_course_submissions.html container_bottom.html footer.html > /app/delete_course_submissions.html
 cat header.html navbar_top.html navbar_bottom.html container_top.html import_course.html container_bottom.html footer.html > /app/import_course.html
 cat header.html navbar_top.html navbar_course.html navbar_assignment.html navbar_problem.html navbar_bottom.html container_top.html assignment.html container_bottom.html footer.html > /app/assignment.html
 cat header.html navbar_top.html navbar_course.html navbar_assignment.html navbar_problem.html navbar_bottom.html container_top.html assignment_admin.html container_bottom.html footer.html > /app/assignment_admin.html
