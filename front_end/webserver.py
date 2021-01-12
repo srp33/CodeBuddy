ba4c98574aeea222d316bbfa3c27083d8aa148a4
Emme Tuft <emmetuft@gmail.com>
2021-01-12 08:30:10 -0700

    Gave administrators access to instructor menu

--- front_end/webserver.py
+++ front_end/webserver.py
@@ -264,13 +264,15 @@ class ProfileAdminHandler(BaseUserHandler):
 class ProfileSelectCourseHandler(BaseUserHandler):
     def get(self, user_id):
         try:
-            if self.is_administrator() or self.is_instructor():
+            if self.is_administrator():
+                courses = content.get_courses()
+            elif self.is_instructor():
                 courses = content.get_courses_connected_to_user(user_id)
 
-                if len(courses) > 1:
-                    self.render("profile_select_course.html", courses=courses)
-                else:
-                    self.render("profile_instructor.html", page="instructor", tab=None, course=courses[0][1], assignments=content.get_assignments(courses[0][1]['id']), assistants=content.get_users_from_role(courses[0][0], "assistant"), result=None, user_info=self.get_user_info(), is_administrator=self.is_administrator(), is_instructor=self.is_instructor())
+            if len(courses) > 1:
+                self.render("profile_select_course.html", courses=courses, page="instructor", user_info=self.get_user_info(), is_administrator=self.is_administrator(), is_instructor=self.is_instructor())
+            else:
+                self.render("profile_instructor.html", page="instructor", tab=None, course=courses[0][1], assignments=content.get_assignments(courses[0][1]['id']), assistants=content.get_users_from_role(courses[0][0], "assistant"), result=None, user_info=self.get_user_info(), is_administrator=self.is_administrator(), is_instructor=self.is_instructor())
         except Exception as inst:
             render_error(self, traceback.format_exc())
 
