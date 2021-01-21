10b4e2b3ab707b4bece9f0131761de15ef1066c9
Emme Tuft <emmetuft@gmail.com>
2021-01-21 08:44:52 -0700

    Fixed bug on profile instructor page

--- front_end/webserver.py
+++ front_end/webserver.py
@@ -267,7 +267,7 @@ class ProfileSelectCourseHandler(BaseUserHandler):
             if len(courses) > 1:
                 self.render("profile_select_course.html", courses=courses, page="instructor", user_info=self.get_user_info(), is_administrator=self.is_administrator(), is_instructor=self.is_instructor())
             else:
-                self.render("profile_instructor.html", page="instructor", tab=None, course=courses[0][1], assignments=content.get_assignments(courses[0][1]['id']), assistants=content.get_users_from_role(courses[0][0], "assistant"), registered_students=content.get_registered_students(courses[0][0]), result=None, user_info=self.get_user_info(), is_administrator=self.is_administrator(), is_instructor=self.is_instructor())
+                self.render("profile_instructor.html", page="instructor", tab=None, course=courses[0][1], assignments=content.get_assignments(courses[0][0]), instructors=content.get_users_from_role(courses[0][0], "instructor"), assistants=content.get_users_from_role(courses[0][0], "assistant"), registered_students=content.get_registered_students(courses[0][0]), result=None, user_info=self.get_user_info(), is_administrator=self.is_administrator(), is_instructor=self.is_instructor_for_course(courses[0][0]))
         except Exception as inst:
             render_error(self, traceback.format_exc())
 
