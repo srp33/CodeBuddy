c6d3b762b33bf7d5cfb69962c990b936e9a7df59
srp33 <srp33@codebuddy.byu.edu>
2021-03-11 16:20:36 -0700

    Fixed problem where ggplot2 images were getting cropped.

--- back_ends/r/exec.R
+++ back_ends/r/exec.R
@@ -1,20 +1,30 @@
 code_file_path = commandArgs()[9]
 output_type = commandArgs()[10]
 
+# Code that didn't work right for both base R graphics and ggplot2
+#exec_jpg <- function(code) {
+#  library(magick)
+#  library(ggplot2)
+#
+#  fig <- image_graph(bg="white", res=150, clip=FALSE)
+#
+#  eval(parse(text=code))
+#
+#  if (!is.null(ggplot2::last_plot()))
+#    print(ggplot2::last_plot())
+#
+#  dev.off()
+#
+#  return(image_write(fig, path="/sandbox/image_output", format="jpg", flatten=FALSE))
+#}
+
 exec_jpg <- function(code) {
-  library(magick)
   library(ggplot2)
 
-  fig <- image_graph(bg="white", res=150, clip=FALSE)
+  pdf(NULL) # Prevents Rplots.pdf from being created.
 
   eval(parse(text=code))
-
-  if (!is.null(ggplot2::last_plot()))
-    print(ggplot2::last_plot())
-
-  dev.off()
-
-  return(image_write(fig, path="/sandbox/image_output", format="jpg", flatten=FALSE))
+  ggsave("/sandbox/image_output", dpi = 150, device = "jpeg")
 }
 
 code <- readChar(code_file_path, file.info(code_file_path)$size)
