273fe6135b1e2b8acd3c2acaa6d937a03e976ad6
Emme Tuft <emmetuft@gmail.com>
2021-03-22 08:59:52 -0600

    Updated move exercise functionality to redirect to the assignment the exercise was moved from

--- front_end/webserver.py
+++ front_end/webserver.py
@@ -1022,11 +1022,9 @@ class MoveExerciseHandler(BaseUserHandler):
 
             new_assignment_id = self.get_body_argument("new_assignment_id")
             content.move_exercise(course, assignment, exercise, new_assignment_id)
+            assignment_basics = content.get_assignment_basics(course, assignment)
 
-            assignment_basics = content.get_assignment_basics(course, new_assignment_id)
-            out_file = f"Assignment_{new_assignment_id}_Scores.csv"
-
-            self.render("assignment_admin.html", courses=content.get_courses(True), assignments=content.get_assignments(course, True), exercises=content.get_exercises(course, new_assignment_id, True), exercise_statuses=content.get_exercise_statuses(course, new_assignment_id, self.get_user_info()["user_id"]), course_basics=content.get_course_basics(course), assignment_basics=assignment_basics, assignment_details=content.get_assignment_details(course, new_assignment_id, True), course_options=[x[1] for x in content.get_courses() if str(x[0]) != course], user_info=self.get_user_info(), is_administrator=self.is_administrator(), is_instructor=self.is_instructor_for_course(course), out_file=out_file)
+            self.render("assignment_admin.html", courses=content.get_courses(True), assignments=content.get_assignments(course, True), exercises=content.get_exercises(course, assignment, True), exercise_statuses=content.get_exercise_statuses(course, assignment, self.get_user_info()["user_id"]), course_basics=content.get_course_basics(course), assignment_basics=assignment_basics, assignment_details=content.get_assignment_details(course, assignment, True), course_options=[x[1] for x in content.get_courses() if str(x[0]) != course], user_info=self.get_user_info(), is_administrator=self.is_administrator(), is_instructor=self.is_instructor_for_course(course), download_file_name=get_scores_download_file_name(assignment_basics))
         except Exception as inst:
             render_error(self, traceback.format_exc())
 
