5636a5d1491912e8d21cb5d6a0b510cc4d95b074
Zach Eliason <eliasonzach@gmail.com>
2021-05-05 16:53:34 -0600

    Client_ip

--- front_end/webserver.py
+++ front_end/webserver.py
@@ -710,6 +710,7 @@ class AssignmentHandler(BaseUserHandler):
                 for address in assignment_details["valid_ip_addresses"]:
                     print("valid ip:",address)
                 if client_ip not in assignment_details["valid_ip_addresses"]:
+                    print("YIKES")
                     self.render("unavailable_assignment.html", courses=content.get_courses(),
                                 assignments=content.get_assignments(course),
                                 course_basics=content.get_course_basics(course),
