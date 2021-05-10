de649cca9d095d9c0d6c4231ef3f3ed8d228a053
Zach Eliason <eliasonzach@gmail.com>
2021-05-10 10:32:19 -0600

    Fix valid_ip_address changes

--- front_end/webserver.py
+++ front_end/webserver.py
@@ -775,7 +775,6 @@ class EditAssignmentHandler(BaseUserHandler):
             if self.get_body_argument("access_restricted") == "Yes":
                 assignment_details["valid_ip_addresses"] = self.get_body_argument("valid_ip_addresses").strip().split(",")
                 assignment_details["valid_ip_addresses"][:] = [x for x in assignment_details["valid_ip_addresses"] if x != "" and x != ","]
-                print(assignment_details["valid_ip_addresses"])
             else:
                 assignment_details["valid_ip_addresses"] = []
 
