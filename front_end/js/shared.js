a8fb56769bbd188074d93e2dbe99c002a8eeb060
Zach Eliason <eliasonzach@gmail.com>
2021-06-01 11:17:06 -0600

    Consolidate js code

--- front_end/js/shared.js
+++ front_end/js/shared.js
@@ -4,10 +4,29 @@ function convertDate(d) {
   var hours = d.getHours();
   var minutes = d.getMinutes() < 10 ? "0" + d.getMinutes():d.getMinutes();
   var suffix = hours >= 12 ? "PM":"AM";
-  var time = ((hours + 11) % 12 + 1) + ":" + minutes + " " + suffix; 
+  var time = ((hours + 11) % 12 + 1) + ":" + minutes + " " + suffix;
 
   var start_date = days[d.getDay()] + " " + (d.getMonth() + 1) + "/" + d.getDate() + "/" + d.getFullYear().toString().substr(-2) +
   " " + time;
   return start_date;
 }
 
+// allow pasting images into markdown fields
+$('div[contenteditable]').bind('paste', function(e) {
+  if (e.originalEvent.clipboardData.items[0].kind != "string") {
+  	var data = e.originalEvent.clipboardData.items[0].getAsFile()
+  	var elem = this;
+  	var fr = new FileReader;
+
+  	fr.onloadend = function() {
+  	  var img = new Image;
+  	  img.src = fr.result;
+  	  img.onload = function() {
+  		  $(elem).append(img);
+  	  };
+  	};
+
+  	fr.readAsDataURL(data);
+  	e.preventDefault();
+  }
+});
