b8030e2769088040e355d1edab1c311622de3b10
Zach Eliason <eliasonzach@gmail.com>
2021-07-08 22:53:21 -0600

    remove test_mod_code

--- back_ends/python_script/test_mod_code_for_jpg.py
+++
@@ -1,9 +0,0 @@
-import sys
-
-i = sys.argv[1]
-
-with open("code.py", "a") as test_file:
-    test_file.write(f"""
-from matplotlib import pyplot as my_plt_saver
-my_plt_saver.savefig('test_image_output_{i}', format='jpg', dpi=150)
-my_plt_saver.close()""")
