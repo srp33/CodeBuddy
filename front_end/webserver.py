0f53d55ef385a5080ee9ecf5e63c3e1e636703ea
Zach Eliason <eliasonzach@gmail.com>
2021-07-09 18:00:41 -0600

    address corrections

--- front_end/webserver.py
+++ front_end/webserver.py
@@ -227,7 +227,7 @@ class ProfileCoursesHandler(BaseUserHandler):
             result = ""
 
             if not consent_given:
-                result = "Error: Cannot register for this course without consent."
+                result = "You may not register for this course without providing consent to participate in the research study. Please review the instructions on what you should do if you decline to participate in the study."
             elif (course_passcode == None or course_passcode == passcode):
                 if (content.course_exists(course_id)):
                     if (content.check_user_registered(course_id, user_id)):
@@ -267,7 +267,7 @@ class ProfileAdminHandler(BaseUserHandler):
     def get (self, user_id):
         try:
             if self.is_administrator():
-                self.render("profile_admin.html", page="admin", tab=None, admins=content.get_users_from_role(0, "administrator"), result=None, user_info=self.get_user_info(), is_administrator=self.is_administrator(), is_instructor=self.is_instructor(), is_assistant=self.is_assistant())
+                self.render("profile_admin.html", page="admin", tab=None, registered_courses=content.get_registered_courses(user_id), admins=content.get_users_from_role(0, "administrator"), result=None, user_info=self.get_user_info(), is_administrator=self.is_administrator(), is_instructor=self.is_instructor(), is_assistant=self.is_assistant())
             else:
                 self.render("permissions.html")
         except Exception as inst:
@@ -434,7 +434,7 @@ class ProfileHelpRequestsHandler(BaseUserHandler):
 class ProfileStudentHelpRequestsHandler(BaseUserHandler):
     def get(self):
         try:
-            self.render("profile_student_help_requests.html", page="help_requests", result=None, user_info=self.get_user_info(), help_requests=content.get_student_help_requests(self.get_user_id()), is_administrator=self.is_administrator(), is_instructor=self.is_instructor(), is_assistant=self.is_assistant())
+            self.render("profile_student_help_requests.html", page="help_requests", result=None, user_info=self.get_user_info(), registered_courses=content.get_registered_courses(self.get_user_id()), help_requests=content.get_student_help_requests(self.get_user_id()), is_administrator=self.is_administrator(), is_instructor=self.is_instructor(), is_assistant=self.is_assistant())
         except Exception as inst:
             render_error(self, traceback.format_exc())
 
@@ -442,7 +442,7 @@ class ProfilePreferencesHandler(BaseUserHandler):
     def get(self, user_id):
         try:
             ace_themes = ["ambiance", "chaos", "chrome", "clouds", "cobalt", "dracula", "github", "kr_theme", "monokai", "sqlserver", "terminal", "tomorrow", "xcode"]
-            self.render("profile_preferences.html", page="preferences", code_completion_path="ace/mode/r", ace_themes=ace_themes, user_info=self.get_user_info(), is_administrator=self.is_administrator(), is_instructor=self.is_instructor(), is_assistant=self.is_assistant())
+            self.render("profile_preferences.html", page="preferences", code_completion_path="ace/mode/r", registered_courses=content.get_registered_courses(user_id), ace_themes=ace_themes, user_info=self.get_user_info(), is_administrator=self.is_administrator(), is_instructor=self.is_instructor(), is_assistant=self.is_assistant())
         except Exception as inst:
             render_error(self, traceback.format_exc())
 
@@ -521,8 +521,8 @@ class EditCourseHandler(BaseUserHandler):
             course_basics["visible"] = self.get_body_argument("is_visible") == "Yes"
             course_details["introduction"] = self.get_body_argument("introduction").strip()
             course_details["passcode"] = self.get_body_argument("passcode").strip()
-            course_details["consent_text"] = self.get_body_argument("consent_form_text").strip()
-            course_details["consent_alternative_text"] = self.get_body_argument("consent_alternative_text").strip()
+            course_details["consent_text"] = self.get_body_argument("consent_form").strip()
+            course_details["consent_alternative_text"] = self.get_body_argument("consent_alternative").strip()
             course_details["enable_research"] = self.get_body_argument("enable_research") == "Yes"
 
             if course_details["passcode"] == "":
