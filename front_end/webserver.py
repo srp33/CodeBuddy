d0c0af84ff92cab507f0f6e67a47ebb1e172a061
Zach Eliason <eliasonzach@gmail.com>
2021-07-22 13:39:23 -0600

    bug fix

--- front_end/webserver.py
+++ front_end/webserver.py
@@ -315,7 +315,7 @@ class ProfileSelectCourseHandler(BaseUserHandler):
                 self.render("profile_select_course.html", courses=courses, page="instructor", user_info=self.get_user_info(), is_administrator=self.is_administrator(), is_instructor=self.is_instructor(), is_assistant=self.is_assistant())
             elif len(courses) == 0:
                 # Prevents CodeBuddy from crashing in the case of zero courses.
-                self.render("unavailable_assignment.html", course_basics={"no_courses_created": True}, assignments=[], user_info=self.get_user_info())
+                self.render("unavailable_assignment.html", course_basics={"exists": False, "no_courses_created": True}, assignments=[], user_info=self.get_user_info())
             else:
                 self.render("profile_instructor.html", page="instructor", tab=None, course=courses[0][1], assignments=content.get_assignments(courses[0][0]), instructors=content.get_users_from_role(courses[0][0], "instructor"), assistants=content.get_users_from_role(courses[0][0], "assistant"), registered_students=content.get_registered_students(courses[0][0]), result=None, user_info=self.get_user_info(), is_administrator=self.is_administrator(), is_instructor=self.is_instructor_for_course(courses[0][0]), is_assistant=self.is_assistant_for_course(courses[0][0]))
         except Exception as inst:
