b2b3d9957863e53a7a6bb67d63ce9d2c35831ef1
Zach Eliason <eliasonzach@gmail.com>
2021-08-05 11:17:15 -0600

    separate ws

--- front_end/webserver.py
+++ front_end/webserver.py
@@ -165,6 +165,33 @@ def make_app():
     return app
 
 
+class StaticFileHandler(RequestHandler):
+    async def get(self, file_name):
+        if file_name.endswith(".html"):
+            try:
+                self.render(file_name)
+            except Exception as inst:
+                render_error(self, traceback.format_exc())
+        else:
+            content_type = "text/css"
+            read_mode = "r"
+
+            if file_name.endswith(".js"):
+                content_type = "text/javascript"
+            elif file_name.endswith(".png"):
+                content_type = "image/png"
+                read_mode = "rb"
+            elif file_name.endswith(".ico"):
+                content_type = "image/x-icon"
+                read_mode = "rb"
+            elif file_name.endswith(".webmanifest"):
+                content_type = "application/json"
+
+            file_contents = read_file("/static/{}".format(file_name), mode=read_mode)
+
+            self.set_header('Content-type', content_type)
+            self.write(file_contents)
+
 
 
 if __name__ == "__main__":
