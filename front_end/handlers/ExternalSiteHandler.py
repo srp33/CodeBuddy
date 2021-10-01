26b8dbe70f53769e4c4baf1bc45ecfbe78f3f125
srp33 <srp33@codebuddy.byu.edu>
2021-10-01 14:34:52 -0600

    Debugging external site handler and added logic to cache external files.

--- front_end/handlers/ExternalSiteHandler.py
+++ front_end/handlers/ExternalSiteHandler.py
@@ -1,4 +1,6 @@
 from BaseUserHandler import *
+import hashlib
+import os
 import urllib.request
 
 class ExternalSiteHandler(RequestHandler):
@@ -6,8 +8,19 @@ class ExternalSiteHandler(RequestHandler):
         try:
             settings_dict = load_yaml_dict(read_file("/Settings.yaml"))
 
+            print(settings_dict["allowed_external_urls"])
             if url in settings_dict["allowed_external_urls"]:
-                content = urllib.request.urlopen(url).read()
+                url_hash = hashlib.md5(url.encode()).hexdigest()
+                cache_file_path = f"/tmp/cache/{url_hash}"
+
+                if os.path.exists(cache_file_path):
+                    with open(cache_file_path, "rb") as cache_file:
+                        content = cache_file.read()
+                else:
+                    content = urllib.request.urlopen(url).read()
+
+                    with open(cache_file_path, "wb") as cache_file:
+                        cache_file.write(content)
 
                 if url.endswith(".html"):
                     self.set_header('Content-type', "text/html; charset=UTF-8")
