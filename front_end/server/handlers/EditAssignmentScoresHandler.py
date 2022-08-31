e2165d1788da0dc4339f0299e22207606c855219
Stephen Piccolo <stephen.piccolo.byu@gmail.com>
2022-08-31 15:44:22 -0600

    Lots of little tweaks to the admin pages to streamline

--- front_end/server/handlers/EditAssignmentScoresHandler.py
+++ front_end/server/handlers/EditAssignmentScoresHandler.py
@@ -1,10 +1,10 @@
 from BaseUserHandler import *
 
-class EditScoresHandler(BaseUserHandler):
+class EditAssignmentScoresHandler(BaseUserHandler):
     def get(self, course, assignment, student_id):
         try:
             if self.is_administrator() or self.is_instructor_for_course(course) or self.is_assistant_for_course(course):
-                self.render("edit_scores.html", student_id=student_id, courses=self.get_courses(), course_basics=self.content.get_course_basics(course), assignments=self.content.get_assignments(course), assignment_basics=self.content.get_assignment_basics(course, assignment), exercises=self.content.get_exercises(course, assignment), exercise_statuses=self.content.get_exercise_statuses(course, assignment, student_id), result=None, user_info=self.get_user_info())
+                self.render("edit_assignment_scores.html", student_id=student_id, courses=self.get_courses(), course_basics=self.content.get_course_basics(course), assignments=self.content.get_assignments(course), assignment_basics=self.content.get_assignment_basics(course, assignment), exercises=self.content.get_exercises(course, assignment), exercise_statuses=self.content.get_exercise_statuses(course, assignment, student_id), result=None, user_info=self.get_user_info())
             else:
                 self.render("permissions.html")
         except Exception as inst:
@@ -23,7 +23,7 @@ class EditScoresHandler(BaseUserHandler):
                     else:
                         result = "Error: Newly entered scores must be numeric."
 
-                self.render("edit_scores.html", student_id=student_id, courses=self.get_courses(), course_basics=self.content.get_course_basics(course), assignments=self.content.get_assignments(course), assignment_basics=self.content.get_assignment_basics(course, assignment), exercises=self.content.get_exercises(course, assignment), exercise_statuses=self.content.get_exercise_statuses(course, assignment, student_id), result=result, user_info=self.get_user_info())
+                self.render("edit_assignment_scores.html", student_id=student_id, courses=self.get_courses(), course_basics=self.content.get_course_basics(course), assignments=self.content.get_assignments(course), assignment_basics=self.content.get_assignment_basics(course, assignment), exercises=self.content.get_exercises(course, assignment), exercise_statuses=self.content.get_exercise_statuses(course, assignment, student_id), result=result, user_info=self.get_user_info())
             else:
                 self.render("permissions.html")
         except Exception as inst:
