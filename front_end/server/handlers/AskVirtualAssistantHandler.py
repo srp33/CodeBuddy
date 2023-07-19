42a40cd94d20b5f5011234afd64be771799bd684
Stephen Piccolo <stephen.piccolo.byu@gmail.com>
2023-07-19 13:48:49 -0600

    Got Virtual Assistant working with OpenAI API

--- front_end/server/handlers/AskVirtualAssistantHandler.py
+++ front_end/server/handlers/AskVirtualAssistantHandler.py
@@ -1,4 +1,5 @@
 from BaseUserHandler import *
+import openai
 
 class AskVirtualAssistantHandler(BaseUserHandler):
     async def post(self, course_id, assignment_id, exercise_id):
@@ -17,19 +18,47 @@ class AskVirtualAssistantHandler(BaseUserHandler):
 
             if out_dict["message"] == "":
                 question = self.get_body_argument("question").strip()
+                student_code = self.get_body_argument("student_code").strip()
 
-                # TODO: OpenAI API
-                response = "This is the response."
+                response = await self.access_openai(course_details, course_id, assignment_id, exercise_id, question, student_code)
 
-                self.content.save_virtual_assistant_interaction(course_id, assignment_id, exercise_id, self.get_current_user(), question, response)
+                # self.content.save_virtual_assistant_interaction(course_id, assignment_id, exercise_id, self.get_current_user(), question, response)
 
                 out_dict["message"] = response
                 out_dict["success"] = True
-        # except ConnectionError as inst:
-        #     out_dict["message"] = "The front-end server was unable to contact the back-end server."
-        # except ReadTimeout as inst:
-        #     out_dict["message"] = f"Your solution timed out after {self.settings_dict['back_ends'][exercise_details['back_end']]['timeout_seconds']} seconds."
         except Exception as inst:
             out_dict["message"] = traceback.format_exc()
 
         self.write(json.dumps(out_dict, default=str))
+
+    async def access_openai(self, course_details, course_id, assignment_id, exercise_id, question_content, student_code):
+        course_basics = await self.get_course_basics(course_id)
+        course_id = course_basics["id"]
+        assignment_basics = await self.get_assignment_basics(course_basics, assignment_id)
+        exercise_details = await self.get_exercise_details(course_basics, assignment_basics, exercise_id)
+
+        openai.api_key = course_details["virtual_assistant_config"]["api_key"]
+
+        # test_prompt = ""
+        # for test in exercise_details["tests"]:
+        #     print(test)
+        #TODO? exercise_details["tests"] (including expected output)
+        #TODO? exercise_details["data_files"] (truncate when necessary)
+
+        system_content = f"You are a helpful assistant who gives a hint or short explanation to a student who is writing {exercise_details['back_end'].split('_')[0]} code. Do *not* provide any code. Only provide one or a few sentences to help the student meet the prompt requirements.\n\n"
+        
+        if len(student_code) > 0:
+             system_content += f"Below is the student's current code:\n\n```{student_code}```\n\n"
+             
+        system_content += f"Below is the prompt that the student is working to address:\n\n{exercise_details['instructions']}"
+
+        messages = [{"role": "system", "content": system_content}, {"role": "user", "content": question_content}]
+
+        response = await openai.ChatCompletion.acreate(
+            model="gpt-4-0314",
+            temperature=0.7,
+            messages=messages,
+            timeout=30
+        )
+
+        return response.choices[0].message.content
