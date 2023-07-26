ed54463ccb814c8127ad8fb37f64e22a9fb1448b
Stephen Piccolo <stephen.piccolo.byu@gmail.com>
2023-07-26 19:54:14 +0200

    Modified settings so that warnings and library loading messages are not shown

--- back_ends/r/exec_code_txt_output.sh
+++ back_ends/r/exec_code_txt_output.sh
@@ -4,12 +4,11 @@
 #   This program is free software: you can redistribute it and/or modify it under the terms of the GNU Affero General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version. This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Affero General Public License for more details. You should have received a copy of the GNU Affero General Public License along with this program.  If not, see <http://www.gnu.org/licenses/>.
 # </copyright_statement>
 
-cp before_code code
-
-echo "options(warn=-1)" >> code
+echo "options(warn=-1)" > code
 echo "options(tidyverse.quiet = TRUE)" >> code
 echo "options(dplyr.summarise.inform = FALSE)" >> code
 
+cat before_code >> code
 cat main_code >> code
 cat after_code >> code
 
