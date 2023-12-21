efc68c5489df0432893f1ba76976f6a9f0cd81d5
Stephen Piccolo <stephen.piccolo.byu@gmail.com>
2023-12-21 07:11:30 -0700

    Saving source image, modifying footer

--- front_end/build_html.sh
+++ front_end/build_html.sh
@@ -17,54 +17,80 @@ rm -f $OUTPUT/*
 
 cd templates
 
+privacy_notice_html=$(grep "^privacy_notice_html: " ../../Settings.yaml | sed "1s/privacy_notice_html: //")
+cookie_information_html=$(grep "^cookie_information_html: " ../../Settings.yaml | sed "1s/cookie_information_html: //")
+
+pwd
+python3 -c """
+privacy_notice_html = '${privacy_notice_html}'.strip()
+cookie_information_html = '${cookie_information_html}'.strip()
+
+if len(privacy_notice_html) == 0:
+    print('You must specify a value for privacy_notice_html in Settings.yaml.')
+    import sys
+    sys.exit(1)
+
+if len(cookie_information_html) == 0:
+    print('You must specify a value for cookie_information_html in Settings.yaml.')
+    import sys
+    sys.exit(1)
+
+with open('footer.html') as footer_file:
+    text = footer_file.read()
+    text = text.replace('{privacy_notice_html}', privacy_notice_html)
+    text = text.replace('{cookie_information_html}', cookie_information_html)
+
+with open('/tmp/footer.html', 'w') as footer_file:
+    footer_file.write(text)"""
+
 for page_name in home about choose_login_option contact_us devlogin
 do
-  # cat header.html navbar_top.html navbar_bottom.html container_color.html ${page_name}.html container_bottom.html footer.html > "$OUTPUT/${page_name}.html"
-  cat header.html container_color.html ${page_name}.html container_bottom.html footer.html > "$OUTPUT/${page_name}.html"
+  # cat header.html navbar_top.html navbar_bottom.html container_color.html ${page_name}.html container_bottom.html /tmp/footer.html > "$OUTPUT/${page_name}.html"
+  cat header.html container_color.html ${page_name}.html container_bottom.html /tmp/footer.html > "$OUTPUT/${page_name}.html"
 done
 
 # Dynamic pages
 
 cat test.html > "$OUTPUT/test.html"
-cat header.html navbar_top.html navbar_menu.html navbar_bottom.html container_color.html courses.html container_bottom.html footer.html > "$OUTPUT/courses.html"
-cat header.html navbar_top.html navbar_menu.html navbar_bottom.html container_color.html available.html container_bottom.html footer.html > "$OUTPUT/available.html"
-cat header.html navbar_top.html navbar_menu.html navbar_bottom.html container_color.html manage_admins.html container_bottom.html footer.html > "$OUTPUT/manage_admins.html"
-cat header.html navbar_top.html navbar_menu.html navbar_bottom.html container_color.html manage_instructors.html container_bottom.html footer.html > "$OUTPUT/manage_instructors.html"
-cat header.html navbar_top.html navbar_menu.html navbar_bottom.html container_color.html manage_assistants.html container_bottom.html footer.html > "$OUTPUT/manage_assistants.html"
-cat header.html navbar_top.html navbar_menu.html navbar_bottom.html container_color.html manage_students.html container_bottom.html footer.html > "$OUTPUT/manage_students.html"
-cat header.html navbar_top.html navbar_menu.html navbar_bottom.html container_color.html manage_users.html container_bottom.html footer.html > "$OUTPUT/manage_users.html"
-cat header.html navbar_top.html navbar_menu.html navbar_bottom.html container_color.html my_profile.html container_bottom.html footer.html > "$OUTPUT/my_profile.html"
-cat header.html javascript_container.html navbar_top.html navbar_menu.html navbar_bottom.html container_color.html preferences.html container_bottom.html footer.html > "$OUTPUT/preferences.html"
-cat header.html navbar_top.html navbar_course.html navbar_assignment.html navbar_menu.html navbar_bottom.html container_color.html course.html container_bottom.html footer.html > "$OUTPUT/course.html"
-cat header.html navbar_top.html navbar_course.html navbar_assignment.html navbar_menu.html navbar_bottom.html container_color.html course_admin.html other_functions.html container_bottom.html footer.html > "$OUTPUT/course_admin.html"
-cat header.html navbar_top.html navbar_course.html navbar_assignment.html navbar_menu.html navbar_bottom.html container_color.html edit_course.html container_bottom.html footer.html > "$OUTPUT/edit_course.html"
-cat header.html navbar_top.html navbar_course.html navbar_assignment.html navbar_exercise.html navbar_menu.html navbar_bottom.html container_color.html assignment.html container_bottom.html footer.html > "$OUTPUT/assignment.html"
-cat header.html navbar_top.html navbar_course.html navbar_assignment.html navbar_exercise.html navbar_menu.html navbar_bottom.html container_color.html assignment_admin.html other_functions.html container_bottom.html footer.html > "$OUTPUT/assignment_admin.html"
-cat header.html navbar_top.html navbar_course.html navbar_assignment.html navbar_exercise.html navbar_menu.html navbar_bottom.html container_color.html edit_assignment.html container_bottom.html footer.html > "$OUTPUT/edit_assignment.html"
-cat header.html navbar_top.html navbar_course.html navbar_assignment.html navbar_menu.html navbar_bottom.html container_color.html resave_exercises.html container_bottom.html footer.html > "$OUTPUT/resave_exercises.html"
-cat header.html javascript_container.html navbar_top.html navbar_course.html navbar_assignment.html navbar_exercise.html navbar_menu.html navbar_bottom.html container_color.html exercise_functions.html exercise.html container_bottom.html footer.html > "$OUTPUT/exercise.html"
-cat header.html javascript_container.html navbar_top.html navbar_course.html navbar_assignment.html navbar_exercise.html navbar_menu.html navbar_bottom.html container_color.html edit_exercise.html container_bottom.html footer.html > "$OUTPUT/edit_exercise.html"
-cat header.html navbar_top.html navbar_course.html navbar_menu.html navbar_bottom.html container_color.html view_at_risk_students.html container_bottom.html footer.html > "$OUTPUT/view_at_risk_students.html"
-cat header.html navbar_top.html navbar_course.html navbar_assignment.html navbar_exercise.html navbar_menu.html navbar_bottom.html container_color.html exercise_functions.html view_instructor_solution.html container_bottom.html footer.html > "$OUTPUT/view_instructor_solution.html"
-cat header.html navbar_top.html navbar_course.html navbar_assignment.html navbar_exercise.html navbar_menu.html navbar_bottom.html container_color.html exercise_functions.html view_peer_solution.html container_bottom.html footer.html > "$OUTPUT/view_peer_solution.html"
-cat header.html navbar_top.html navbar_course.html navbar_assignment.html navbar_exercise.html navbar_menu.html navbar_bottom.html container_color.html view_assignment_scores.html container_bottom.html footer.html > "$OUTPUT/view_assignment_scores.html"
-cat header.html navbar_top.html navbar_course.html navbar_assignment.html navbar_menu.html navbar_bottom.html container_color.html view_student_assignment_scores.html container_bottom.html footer.html > "$OUTPUT/view_student_assignment_scores.html"
-cat header.html navbar_top.html navbar_course.html navbar_assignment.html navbar_exercise.html navbar_menu.html navbar_bottom.html container_color.html edit_assignment_scores.html container_bottom.html footer.html > "$OUTPUT/edit_assignment_scores.html"
-cat header.html javascript_container.html navbar_top.html navbar_course.html navbar_assignment.html navbar_exercise.html navbar_menu.html navbar_bottom.html container_color.html exercise_functions.html student_exercise.html container_bottom.html footer.html > "$OUTPUT/student_exercise.html"
-cat header.html navbar_top.html navbar_course.html navbar_assignment.html navbar_menu.html navbar_bottom.html container_color.html view_exercise_scores.html container_bottom.html footer.html > "$OUTPUT/view_exercise_scores.html"
-cat header.html navbar_top.html navbar_course.html navbar_assignment.html navbar_menu.html navbar_bottom.html container_color.html exercise_submissions.html container_bottom.html footer.html > "$OUTPUT/exercise_submissions.html"
-cat header.html navbar_top.html navbar_menu.html navbar_bottom.html container_color.html summarize_logs.html container_bottom.html footer.html > "$OUTPUT/summarize_logs.html"
-cat header.html navbar_top.html navbar_menu.html navbar_bottom.html container_color.html delete_user.html container_bottom.html footer.html > "$OUTPUT/delete_user.html"
+cat header.html navbar_top.html navbar_menu.html navbar_bottom.html container_color.html courses.html container_bottom.html /tmp/footer.html > "$OUTPUT/courses.html"
+cat header.html navbar_top.html navbar_menu.html navbar_bottom.html container_color.html available.html container_bottom.html /tmp/footer.html > "$OUTPUT/available.html"
+cat header.html navbar_top.html navbar_menu.html navbar_bottom.html container_color.html manage_admins.html container_bottom.html /tmp/footer.html > "$OUTPUT/manage_admins.html"
+cat header.html navbar_top.html navbar_menu.html navbar_bottom.html container_color.html manage_instructors.html container_bottom.html /tmp/footer.html > "$OUTPUT/manage_instructors.html"
+cat header.html navbar_top.html navbar_menu.html navbar_bottom.html container_color.html manage_assistants.html container_bottom.html /tmp/footer.html > "$OUTPUT/manage_assistants.html"
+cat header.html navbar_top.html navbar_menu.html navbar_bottom.html container_color.html manage_students.html container_bottom.html /tmp/footer.html > "$OUTPUT/manage_students.html"
+cat header.html navbar_top.html navbar_menu.html navbar_bottom.html container_color.html manage_users.html container_bottom.html /tmp/footer.html > "$OUTPUT/manage_users.html"
+cat header.html navbar_top.html navbar_menu.html navbar_bottom.html container_color.html my_profile.html container_bottom.html /tmp/footer.html > "$OUTPUT/my_profile.html"
+cat header.html javascript_container.html navbar_top.html navbar_menu.html navbar_bottom.html container_color.html preferences.html container_bottom.html /tmp/footer.html > "$OUTPUT/preferences.html"
+cat header.html navbar_top.html navbar_course.html navbar_assignment.html navbar_menu.html navbar_bottom.html container_color.html course.html container_bottom.html /tmp/footer.html > "$OUTPUT/course.html"
+cat header.html navbar_top.html navbar_course.html navbar_assignment.html navbar_menu.html navbar_bottom.html container_color.html course_admin.html other_functions.html container_bottom.html /tmp/footer.html > "$OUTPUT/course_admin.html"
+cat header.html navbar_top.html navbar_course.html navbar_assignment.html navbar_menu.html navbar_bottom.html container_color.html edit_course.html container_bottom.html /tmp/footer.html > "$OUTPUT/edit_course.html"
+cat header.html navbar_top.html navbar_course.html navbar_assignment.html navbar_exercise.html navbar_menu.html navbar_bottom.html container_color.html assignment.html container_bottom.html /tmp/footer.html > "$OUTPUT/assignment.html"
+cat header.html navbar_top.html navbar_course.html navbar_assignment.html navbar_exercise.html navbar_menu.html navbar_bottom.html container_color.html assignment_admin.html other_functions.html container_bottom.html /tmp/footer.html > "$OUTPUT/assignment_admin.html"
+cat header.html navbar_top.html navbar_course.html navbar_assignment.html navbar_exercise.html navbar_menu.html navbar_bottom.html container_color.html edit_assignment.html container_bottom.html /tmp/footer.html > "$OUTPUT/edit_assignment.html"
+cat header.html navbar_top.html navbar_course.html navbar_assignment.html navbar_menu.html navbar_bottom.html container_color.html resave_exercises.html container_bottom.html /tmp/footer.html > "$OUTPUT/resave_exercises.html"
+cat header.html javascript_container.html navbar_top.html navbar_course.html navbar_assignment.html navbar_exercise.html navbar_menu.html navbar_bottom.html container_color.html exercise_functions.html exercise.html container_bottom.html /tmp/footer.html > "$OUTPUT/exercise.html"
+cat header.html javascript_container.html navbar_top.html navbar_course.html navbar_assignment.html navbar_exercise.html navbar_menu.html navbar_bottom.html container_color.html edit_exercise.html container_bottom.html /tmp/footer.html > "$OUTPUT/edit_exercise.html"
+cat header.html navbar_top.html navbar_course.html navbar_menu.html navbar_bottom.html container_color.html view_at_risk_students.html container_bottom.html /tmp/footer.html > "$OUTPUT/view_at_risk_students.html"
+cat header.html navbar_top.html navbar_course.html navbar_assignment.html navbar_exercise.html navbar_menu.html navbar_bottom.html container_color.html exercise_functions.html view_instructor_solution.html container_bottom.html /tmp/footer.html > "$OUTPUT/view_instructor_solution.html"
+cat header.html navbar_top.html navbar_course.html navbar_assignment.html navbar_exercise.html navbar_menu.html navbar_bottom.html container_color.html exercise_functions.html view_peer_solution.html container_bottom.html /tmp/footer.html > "$OUTPUT/view_peer_solution.html"
+cat header.html navbar_top.html navbar_course.html navbar_assignment.html navbar_exercise.html navbar_menu.html navbar_bottom.html container_color.html view_assignment_scores.html container_bottom.html /tmp/footer.html > "$OUTPUT/view_assignment_scores.html"
+cat header.html navbar_top.html navbar_course.html navbar_assignment.html navbar_menu.html navbar_bottom.html container_color.html view_student_assignment_scores.html container_bottom.html /tmp/footer.html > "$OUTPUT/view_student_assignment_scores.html"
+cat header.html navbar_top.html navbar_course.html navbar_assignment.html navbar_exercise.html navbar_menu.html navbar_bottom.html container_color.html edit_assignment_scores.html container_bottom.html /tmp/footer.html > "$OUTPUT/edit_assignment_scores.html"
+cat header.html javascript_container.html navbar_top.html navbar_course.html navbar_assignment.html navbar_exercise.html navbar_menu.html navbar_bottom.html container_color.html exercise_functions.html student_exercise.html container_bottom.html /tmp/footer.html > "$OUTPUT/student_exercise.html"
+cat header.html navbar_top.html navbar_course.html navbar_assignment.html navbar_menu.html navbar_bottom.html container_color.html view_exercise_scores.html container_bottom.html /tmp/footer.html > "$OUTPUT/view_exercise_scores.html"
+cat header.html navbar_top.html navbar_course.html navbar_assignment.html navbar_menu.html navbar_bottom.html container_color.html exercise_submissions.html container_bottom.html /tmp/footer.html > "$OUTPUT/exercise_submissions.html"
+cat header.html navbar_top.html navbar_menu.html navbar_bottom.html container_color.html summarize_logs.html container_bottom.html /tmp/footer.html > "$OUTPUT/summarize_logs.html"
+cat header.html navbar_top.html navbar_menu.html navbar_bottom.html container_color.html delete_user.html container_bottom.html /tmp/footer.html > "$OUTPUT/delete_user.html"
 
-cat header.html diff_txt.html footer.html > "$OUTPUT/diff_txt.html"
-cat header.html diff_jpg.html footer.html > "$OUTPUT/diff_jpg.html"
+cat header.html diff_txt.html /tmp/footer.html > "$OUTPUT/diff_txt.html"
+cat header.html diff_jpg.html /tmp/footer.html > "$OUTPUT/diff_jpg.html"
 
-cat header.html navbar_top.html navbar_bottom.html container_color.html error.html container_bottom.html footer.html > "$OUTPUT/error.html"
-cat header.html navbar_top.html navbar_bottom.html container_color.html permissions.html container_bottom.html footer.html > "$OUTPUT/permissions.html"
-cat header.html navbar_top.html navbar_course.html navbar_assignment.html navbar_menu.html navbar_bottom.html container_color.html unavailable_assignment.html container_bottom.html footer.html > "$OUTPUT/unavailable_assignment.html"
-cat header.html navbar_top.html navbar_course.html navbar_menu.html navbar_bottom.html container_color.html unavailable_course.html container_bottom.html footer.html > "$OUTPUT/unavailable_course.html"
+cat header.html navbar_top.html navbar_bottom.html container_color.html error.html container_bottom.html /tmp/footer.html > "$OUTPUT/error.html"
+cat header.html navbar_top.html navbar_bottom.html container_color.html permissions.html container_bottom.html /tmp/footer.html > "$OUTPUT/permissions.html"
+cat header.html navbar_top.html navbar_course.html navbar_assignment.html navbar_menu.html navbar_bottom.html container_color.html unavailable_assignment.html container_bottom.html /tmp/footer.html > "$OUTPUT/unavailable_assignment.html"
+cat header.html navbar_top.html navbar_course.html navbar_menu.html navbar_bottom.html container_color.html unavailable_course.html container_bottom.html /tmp/footer.html > "$OUTPUT/unavailable_course.html"
 
-cat spa_header.html navbar_top.html navbar_course.html navbar_assignment.html navbar_exercise.html navbar_menu.html navbar_bottom.html spa.html footer.html > "$OUTPUT/spa.html"
+cat spa_header.html navbar_top.html navbar_course.html navbar_assignment.html navbar_exercise.html navbar_menu.html navbar_bottom.html spa.html /tmp/footer.html > "$OUTPUT/spa.html"
 
 for f in "$OUTPUT"/*.html
 do
