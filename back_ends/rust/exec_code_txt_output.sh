d704e87cd66753de983d52414931467b80b96d6a
Stephen Piccolo <stephen.piccolo.byu@gmail.com>
2024-07-19 16:08:31 -0600

    No longer saving Cargo.toml when code is executed.

--- back_ends/rust/exec_code_txt_output.sh
+++ back_ends/rust/exec_code_txt_output.sh
@@ -8,16 +8,15 @@ cp ./* /app/myapp
 
 cd /app/myapp
 
-echo "[package]" > Cargo.toml
-echo "name = \"test-project\"" >> Cargo.toml
-echo "version = \"0.1.0\"" >> Cargo.toml
-echo "authors = [\"ChatGPT\"]" >> Cargo.toml
-echo "edition = \"2021\"" >> Cargo.toml
-echo  >> Cargo.toml
-echo "[dependencies]" >> Cargo.toml
-echo "regex = \"1.10.5\"" >> Cargo.toml
+#echo "[package]" > Cargo.toml
+#echo "name = \"test-project\"" >> Cargo.toml
+#echo "version = \"0.1.0\"" >> Cargo.toml
+#echo "authors = [\"ChatGPT\"]" >> Cargo.toml
+#echo "edition = \"2021\"" >> Cargo.toml
+#echo  >> Cargo.toml
+#echo "[dependencies]" >> Cargo.toml
+#echo "regex = \"1.10.5\"" >> Cargo.toml
 
-#mkdir -p src
 cat before_code main_code after_code > src/main.rs
 
 cargo run --quiet
